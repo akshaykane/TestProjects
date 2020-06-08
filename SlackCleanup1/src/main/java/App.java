6de5ea5cb13774d9b6ec3398fe7cc3d90204f02c
Kane <akkane@ptc.com>
2020-06-08 14:12:31 +0530

    Small code change

--- SlackCleanup1/src/main/java/App.java
+++ SlackCleanup1/src/main/java/App.java
@@ -12,17 +12,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        System.setProperty("webdriver.chrome.driver", "chrome/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://ptcsymphonydev.slack.com/");
-        
-        // Enter username
-        driver.findElement(By.id("email")).clear();
-        driver.findElement(By.id("email")).sendKeys("twx-symphony-dev@ptc.com");
-        
-        // Enter password
-        driver.findElement(By.id("email")).clear();
-        driver.findElement(By.id("email")).sendKeys("twx-symphony-dev@ptc.com");
-        
     }
 }
